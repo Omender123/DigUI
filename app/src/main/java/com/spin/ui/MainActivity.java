29e648a618e548e97142627c332e573f43dc417f
Omender <Os7290915329@gmail.com>
2021-11-06 16:39:31 +0530

    commit

--- app/src/main/java/com/spin/ui/MainActivity.java
+++ app/src/main/java/com/spin/ui/MainActivity.java
@@ -3,12 +3,25 @@ package com.spin.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+TextView btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"Done Successfully",Snackbar.LENGTH_SHORT).show();
+            }
+        });
     }
 }
